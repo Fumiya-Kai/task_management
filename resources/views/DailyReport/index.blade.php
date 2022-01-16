50420385afa6a594d93381e2f3582cc925e49dad
Fumiya-Kai <bunjyo.science@icloud.com>
2022-01-16 18:06:38 +0900

    add: 日報一覧画面の追加、日報テーブル修正

---
+++ resources/views/DailyReport/index.blade.php
@@ -0,0 +1,27 @@
+@extends('common.user')
+@section('content')
+
+<h1 class="page-title">日報</h1>
+<div class="contents-wrapper">
+  <div class="btn"><a href="{{ route('report.create') }}" class="add-link">Add</a></div>
+  <table class="targets-table">
+    <thead class="thead-index">
+      <tr class="row-index tr-index">
+        <th class="col col1">日付</th>
+        <th class="col col2">タイトル</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($reports as $report)
+      <tr class="row-index tr-index target-data" data-report-id="{{ $report->id }}">
+        <td class="col col1" width="5%">{{ $report->report_time->format('m/d(D)')}}</td>
+        <td class="col col3" width="45%">{{ $report->title }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  <div class="paginater">
+  </div>
+</div>
+
+@endsection
