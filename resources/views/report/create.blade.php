1e651c04d60f43fd0f5591de8b307ea43aba7772
Fumiya-Kai <bunjyo.science@icloud.com>
2022-01-16 18:37:09 +0900

    add: 日報新規作成画面の作成

---
+++ resources/views/report/create.blade.php
@@ -0,0 +1,18 @@
+@extends('common.user')
+@section('content')
+
+<h1 class="page-title">目標新規作成</h1>
+<div class="contents-wrapper">
+  <form class="clearfix" action="{{ route('report.store') }}" method="POST">
+    @csrf
+    <p class="label">開始日</p>
+    <input class="form-content" name="report_time" type="date">
+    <p class="label">タイトル</p>
+    <input class="form-content text-form title-form" name="title" type="text" placeholder="日報のタイトルを入力">
+    <p class="label">詳細</p>
+    <textarea class="form-content text-form detail-form" name="content" cols="50" rows="10" placeholder="日報の詳細を入力"></textarea>
+    <button class="btn btn-create" type="submit">作成</button>
+  </form>
+</div>
+
+@endsection
