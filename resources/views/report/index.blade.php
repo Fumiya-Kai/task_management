9efc992b608e612fa086864f89f7ce4f9b44d6c4
Fumiya-Kai <bunjyo.science@icloud.com>
2022-01-16 18:32:59 +0900

    fix: 日報一覧のカラム幅を修正

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -7,15 +7,15 @@
   <table class="targets-table">
     <thead class="thead-index">
       <tr class="row-index tr-index">
-        <th class="col col1">日付</th>
-        <th class="col col2">タイトル</th>
+        <th class="col col1" width="30%">日付</th>
+        <th class="col col2" width="70%">タイトル</th>
       </tr>
     </thead>
     <tbody>
       @foreach ($reports as $report)
       <tr class="row-index tr-index target-data" data-report-id="{{ $report->id }}">
-        <td class="col col1" width="5%">{{ $report->report_time->format('m/d(D)')}}</td>
-        <td class="col col3" width="45%">{{ $report->title }}</td>
+        <td class="col col1" width="30%">{{ $report->report_time->format('m/d(D)')}}</td>
+        <td class="col col2" width="70%">{{ $report->title }}</td>
       </tr>
       @endforeach
     </tbody>
